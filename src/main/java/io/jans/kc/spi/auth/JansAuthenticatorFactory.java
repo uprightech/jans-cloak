8006165e45719af50a5289901d6f1075668fb139
Rolain Djeumen <uprightech@gmail.com>
2023-07-03 08:32:09 +0100

    chore: initial implementation of authenticator factory

--- src/main/java/io/jans/kc/spi/auth/JansAuthenticatorFactory.java
+++ src/main/java/io/jans/kc/spi/auth/JansAuthenticatorFactory.java
@@ -1,5 +1,6 @@
 package io.jans.kc.spi.auth;
 
+import java.util.Arrays;
 import java.util.List; 
 
 import org.keycloak.authentication.Authenticator;
@@ -12,22 +13,47 @@ import org.keycloak.models.KeycloakSession;
 import org.keycloak.models.KeycloakSessionFactory;
 
 import org.keycloak.provider.ProviderConfigProperty;
+import org.keycloak.provider.ProviderConfigurationBuilder;
 
 
 public class JansAuthenticatorFactory implements AuthenticatorFactory {
     
     private static final String PROVIDER_ID = "jans-cloak-authenticator";
+    private static final String DISPLAY_TYPE = "Janssen Authentication";
+    private static final String REFERENCE_CATEGORY = "Janssen Authentication";
+    private static final String HELP_TEXT= "A third-party authenticator for Janssen Auth";
+
+    private static final AuthenticationExecutionModel.Requirement[] REQUIREMENT_CHOICES = {
+        AuthenticationExecutionModel.Requirement.REQUIRED,
+        AuthenticationExecutionModel.Requirement.ALTERNATIVE,
+        AuthenticationExecutionModel.Requirement.DISABLED
+    };
+
+    private static final ProviderConfigProperty JANS_CLIENT_ID_PROP = new ProviderConfigProperty(
+            "jans.auth.client.id",
+            "Janssen Client ID",
+            "Client ID of the OpenID Client created in Janssen-Auth",
+            ProviderConfigProperty.STRING_TYPE,
+            null, 
+            false);
+    
+    private static final List<ProviderConfigProperty> CONFIG_PROPS = ProviderConfigurationBuilder
+            .create()
+            .property(JANS_CLIENT_ID_PROP)
+            .build();
+    
+    private static final JansAuthenticator INSTANCE = new JansAuthenticator();
 
     @Override
     public String getId() {
 
-        return null;
+        return PROVIDER_ID;
     }
 
     @Override
     public Authenticator create(KeycloakSession session) {
 
-        return null;
+        return INSTANCE;
     }
 
     @Override
@@ -51,41 +77,41 @@ public class JansAuthenticatorFactory implements AuthenticatorFactory {
     @Override
     public String getDisplayType() {
 
-        return null;
+        return DISPLAY_TYPE;
     }
 
     @Override
     public String getReferenceCategory() {
 
-        return null;
+        return REFERENCE_CATEGORY;
     }
 
     @Override
     public boolean isConfigurable() {
 
-        return false;
+        return true;
     }
 
     public AuthenticationExecutionModel.Requirement[] getRequirementChoices() {
 
-        return null;
+        return REQUIREMENT_CHOICES;
     }
 
     @Override
     public boolean isUserSetupAllowed() {
 
-        return false;
+        return true;
     }
 
     @Override
     public String getHelpText() {
 
-        return null;
+        return HELP_TEXT;
     }
 
     @Override
     public List<ProviderConfigProperty> getConfigProperties() {
 
-        return null;
+        return CONFIG_PROPS;
     }
 }
